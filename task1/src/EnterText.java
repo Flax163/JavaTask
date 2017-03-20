7609dcd33b97ebd46d225e8d09f90af344120558
Dima Kosych <dipp962@gmail.com>
2017-03-20 22:05:04 +0400

    first commit

--- task1/src/EnterText.java
+++ task1/src/EnterText.java
@@ -2,22 +2,25 @@
  * Created by admin on 17.03.2017.
  */
 import java.util.Scanner;
-class Students{
-
-}
-class Group{
-
-}
+import group.*;
+import student.*;
 public class EnterText {
     public static void main(String[] args)
     {
-        // создаю сущности
-        Students st = new Students();
         Group gr = new Group();
+        Student st = new Student();
+
         // просит ввести текст и выводит его
         Scanner in = new Scanner(System.in);
-        System.out.println("Введите текст: ");
-        String text = in.nextLine();
-        System.out.println(text);
+        System.out.println("Для добавления студента введите - 1" + "\n" + "Для добавления группы введите - 2");
+        String number = in.nextLine();
+        switch (number){
+            case "1":
+                System.out.println("Введите студента");
+                break;
+            case "2":
+                System.out.println("Введите группу");
+                break;
+        }
     }
 }
---
+++ task1/src/student/Student.java
@@ -0,0 +1,25 @@
+package student;
+
+/**
+ * Created by admin on 20.03.2017.
+ */
+public class Student {
+    private int id;
+    private String name;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
